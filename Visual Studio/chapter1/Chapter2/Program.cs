22c98f8381d63923e5781161018d847f7e88c74b
isi <isi@751FJW2>
2022-07-06 10:33:25 -0400

    exercices chapter1(all), chapter 2, types et variables, instructions de controle

---
+++ Visual Studio/chapter1/Chapter2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Question11();
+            Question12();
+            Question13();
+            Question21();
+        }
+        static void Question11()
+        {
+            double v1 = 54.130;
+            sbyte v2 = -115;
+            int v3 = 4825932;
+            byte v4 = 97;
+            short v5 = -10000;
+            short v6 = 20000;
+            byte v7 = 224;
+            int v8 = 970700000;
+            byte v9 = 112;
+            sbyte v10 = -44;
+            int v11 = -1000000;
+            short v12 = 1990;
+            ulong v13 = 123456789123456789;
+        }
+
+        static void Question12()
+        {
+            bool isMale = true;
+        }
+
+        static void Question13()
+        {
+            string hello = "Hello";
+            string world = "World";
+            object result = hello + world;
+            Console.WriteLine(result.ToString());
+        }
+
+        static void Question21()
+        {
+            Console.Write("Give a mark(integer)");
+            int mark = int.Parse(Console.ReadLine()), bonus = 0;
+            switch (mark)
+            {
+                case 1:
+                    
+                case 2:
+                   
+                case 3:
+                    bonus = mark * 10;
+                    break;
+                case 4:
+                case 5:
+                case 6:
+                    bonus = mark * 100;
+                    break;
+                case 7:
+                case 8:
+                    bonus = mark * 1000;
+                    break;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+
+            }
+           
+               
+        }
+
+
+    }
+}
---
+++ Visual Studio/chapter1/chapter1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chapter1
+{
+    class Program
+    {
+        public const double PI = 3.1415926535897931;
+        static void Main(string[] args)
+        {
+            // Question1();
+            //Question2();
+            //Question3();
+        }
+
+        static void Question1()
+        {
+            Console.Write("Please enter the radius of the circle: ");
+            double r = Double.Parse(Console.ReadLine());
+            Console.WriteLine("Hello,Eh, the perimeter of a circle of radius {0} is {1} and its area is {2}   !", r, 2 * PI * r, PI * Math.Pow(r, 2.0));
+        }
+
+        static void Question2()
+        {
+            Console.Write("Please enter the informations about th compagny ");
+            string name, adress, tel, fax;
+            Console.Write("Name ");
+            name = Console.ReadLine();
+            Console.Write("Adress ");
+            adress = Console.ReadLine();
+            Console.Write("Telephone ");
+            tel = Console.ReadLine();
+            Console.Write("Fax ");
+            fax = Console.ReadLine();
+            Console.WriteLine("The compagny {0} has an adress {1} an phone number  {2}, and receive correspondances at fax {3}  ", name, adress, tel, fax);
+        }
+
+        static void Question3()
+        {
+            Console.Write("How many integers do you want to sum? ");
+            int nb = int.Parse(Console.ReadLine()), sum = 0;
+            for (int i = 0; i < nb; i++)
+            {
+                Console.WriteLine("Nombre {0}:", i + 1);
+                sum += int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Le total de ces {0} nombres est de {1}", nb, sum);
+        }
+    }
+
+}
