01023cf8bf4a0f6746de436f9d665b7c11e1cad6
isi <isi@751FJW2>
2022-07-07 12:23:36 -0400

    Chapter 2, arrays

--- Visual Studio/Chapter2/Program.cs
+++ Visual Studio/Chapter2/Program.cs
@@ -8,15 +8,17 @@ namespace Chapter2
 {
     class Program
     {
-        
+
         static void Main(string[] args)
         {
             /*Question11();
             Question12();
             Question13();
-            Question21();*/
-            Chat.Question3();
-         }
+            Question21();
+            Chat.Question3(); 
+            Question33();*/
+            Question32();
+        }
         static void Question11()
         {
             double v1 = 54.130;
@@ -54,9 +56,9 @@ namespace Chapter2
             switch (mark)
             {
                 case 1:
-                    
+
                 case 2:
-                   
+
                 case 3:
                     bonus = mark * 10;
                     break;
@@ -74,8 +76,118 @@ namespace Chapter2
                     break;
 
             }
-           
-               
+        }
+
+        static void Question31()
+        {
+            int nbElmt = 20;
+            int[] tab = new int[nbElmt];
+            for (int i = 0; i < nbElmt; i++)
+            {
+                tab[i] = i * 5;
+                Console.Write(tab[i] + " ");
+            }
+        }
+
+        static void Question32()
+        {
+            Console.Write(" La taille du premier tableau ");
+            int t1 = int.Parse(Console.ReadLine());
+            int[] tab1 = getTab(t1);
+          //  print(tab1);
+            Console.Write(" La taille du second tableau ");
+            int t2 = int.Parse(Console.ReadLine());
+            int[] tab2 = getTab(t2);
+           // print(tab2);
+            bool equality = (tab1.Length == tab2.Length);
+            if (equality)
+            {
+                tab1=triBulle(tab1);
+                print(tab1);
+                tab2=triBulle(tab2);
+                print(tab2);
+            }
+            int i = 0;
+            while (equality && i < tab2.Length)
+            {
+                equality = (tab1[i] == tab2[i]);
+                i++;
+            }
+            if (equality)
+                Console.WriteLine("Egalite");
+            else
+                Console.WriteLine("Inegalite");
+        }
+        /// <summary>
+        /// initializes as an array of nbElmt integers
+        /// </summary>
+        /// <param name="nbElmt"></param>
+        /// <returns></returns>
+        static int[] getTab(int nbElmt)
+        {
+            Console.WriteLine("Donnez les valeurs du tableau");
+            int[] tab = new int[nbElmt];
+            for (int i = 0; i < nbElmt; i++)
+            {
+                Console.Write(" {0} :  ", i + 1);
+                tab[i] = int.Parse(Console.ReadLine());
+            }
+            return tab;
+        }
+        static void print(int[] tab)
+        {
+            for (int i = 0; i < tab.Length; i++)
+            {
+                Console.Write(" {0} ", tab[i]);
+            }
+            Console.WriteLine();
+        }
+        /// <summary>
+        /// asks for the size of an array, initializes it with integers, sorts it and displays the sorted array
+        /// </summary>
+        static void Question33()
+        {
+            Console.Write(" La taille du tableau ");
+            int nbElmt = int.Parse(Console.ReadLine());
+            
+            int[] tab = getTab(nbElmt);
+            // Collecte des valeurs du tableau
+            // Trie
+            triBulle(tab);
+            // Affichage tableau trie
+            print(tab);
+
+        }
+        /// <summary>
+        /// sorts the bubble on the array of integers tab
+        /// </summary>
+        /// <param name="tab"></param>
+        static int[] triBulle(int[] tab)
+        {
+            for (int i = 0; i < tab.Length ; i++)
+            {
+                for (int j = 0; j < tab.Length - i - 1; j++)
+                {
+                   
+                    if (tab[j] > tab[j + 1])
+                    {
+                        permut(tab, j, j+1);
+                    }
+                }
+            }
+            return tab;
+        }
+        /// <summary>
+        /// swaps the values of the array tab positioned at indices a and b
+        /// </summary>
+        /// <param name="tab"></param>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        static void permut(int[] tab,int a, int b)
+        {
+            tab[a] = tab[a] + tab[b];
+            tab[b] = tab[a] - tab[b];
+            tab[a] = tab[a] - tab[b];
         }
 
 
