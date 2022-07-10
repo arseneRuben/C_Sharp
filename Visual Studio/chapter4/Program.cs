aedc9b1d9ae18c44dc6c7d562e04c3edfd04ad3f
isi <isi@751FJW2>
2022-07-10 17:06:26 -0400

    Chapter 4, exercice 2

--- Visual Studio/chapter4/Program.cs
+++ Visual Studio/chapter4/Program.cs
@@ -10,7 +10,8 @@ namespace chapter4
     {
         static void Main(string[] args)
         {
-            test();
+            //Question1();
+            Question2();
         }
 
         static  int GetMax(int a, int b)
@@ -28,11 +29,60 @@ namespace chapter4
             return max;
         }
 
-        static void test()
+        static void Question1()
         {
             Console.WriteLine( 20 == GetMax(-2,3,20) ? "JUSTE" : "FAUX");
             Console.WriteLine(0 == GetMax(-2, -3, 0) ? "JUSTE" : "FAUX");
             Console.WriteLine(2 == GetMax(-2, 2, -20) ? "JUSTE" : "FAUX");
+        }
+
+        static void Question2()
+        {
+            do
+            {
+                Console.WriteLine("Donner une valeur entiere (-1 pour terminer)");
+                int nb = int.Parse(Console.ReadLine());
+                if (nb == -1)
+                    break;
+                else
+                {
+                    switch (nb % 10)
+                    {
+
+
+                        case 0:
+                            Console.WriteLine("ZERO");
+                            break;
+                        case 1:
+                            Console.WriteLine("UN");
+                            break;
+                        case 2:
+                            Console.WriteLine("DEUX");
+                            break;
+                        case 3:
+                            Console.WriteLine("TROIS");
+                            break;
+                        case 4:
+                            Console.WriteLine("QUATRE");
+                            break;
+                        case 5:
+                            Console.WriteLine("CINQ");
+                            break;
+                        case 6:
+                            Console.WriteLine("SIX");
+                            break;
+                        case 7:
+                            Console.WriteLine("SEPT");
+                            break;
+                        case 8:
+                            Console.WriteLine("HUIT");
+                            break;
+                        case 9:
+                            Console.WriteLine("NEUF");
+                            break;
+                    }
+                }
+            } while (true);
         }
     }
 }
